a9d151fb27300f3c633397d9bf5e80d755c50bd5
Henri Karhu <karhu.henri@gmail.com>
2016-12-19 21:06:44 +0200

    ensimmäinen

---
+++ src/main/java/wad/MyFarmApplication.java
@@ -0,0 +1,12 @@
+
+package wad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFarmApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MyFarmApplication.class, args);
+    }
+}
